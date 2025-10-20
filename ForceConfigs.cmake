3c311e97eeb375859993fe3e66bcd0d031644e98
inkgnt <120864571+inkgnt@users.noreply.github.com>
2025-10-20 04:05:48 +0300

    Deleted unused file

--- ForceConfigs.cmake
+++
@@ -1,3 +0,0 @@
-# Force all configuration types early, before Qt overrides anything.
-set(CMAKE_CONFIGURATION_TYPES "Debug;Release;RelWithDebInfo;MinSizeRel" CACHE STRING "" FORCE)
-message(STATUS "✅ Forced configuration types from ForceConfigs.cmake: ${CMAKE_CONFIGURATION_TYPES}")
